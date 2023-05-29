22ef160f35fefb3426a77672c2153c61796b75b4
nsshakula <shakula.n@yandex.ru>
2023-05-30 00:23:33 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let message = () => console.log("Я учу JavaScript!");
+message();
+
+const galleryCard = document.getElementById("flower");
+
+function addBtnBack() {
+  flower.src = "./assets/image/snowdrop.jpg";
+}
+
+function addBtnForward() {
+  flower.src = "./assets/image/primroses.jpg";
+}
